7a8b3b07d0dd08b5d89b85bdf3054149842c9c91
dunky-star <dukygeoffrey39@gmail.com>
2023-02-06 20:57:15 +0300

    OneToOne r/ship

--- src/test/java/com/dunky/in21hours/StudentRepoTest.java
+++ src/test/java/com/dunky/in21hours/StudentRepoTest.java
@@ -1,5 +1,6 @@
 package com.dunky.in21hours;
 
+import com.dunky.in21hours.entity.Passport;
 import com.dunky.in21hours.entity.Student;
 import com.dunky.in21hours.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
@@ -12,14 +13,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class StudentRepoTests {
-
     @Autowired
     StudentRepository studentRepo;
-
     @Autowired
     EntityManager em;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -30,11 +30,24 @@ class StudentRepoTests {
 
     @Test
     @Transactional
+    public void saveStudentWithPassport() {
+        Passport passport = new Passport("Z123456");
+        em.persist(passport);
+
+        Student student = new Student("Mike", "Kampala", new Date());
+
+        student.setPassport(passport);
+        em.persist(student);
+    }
+
+    @Test
+    @Transactional
     public void retrieveStudentAndPassportDetails() {
-        Student student = em.find(Student.class, 20001L);
+        Student student = em.find(Student.class, 10001L);
         logger.info("student -> {}", student);
         logger.info("passport -> {}", student.getPassport());
     }
+
 
 
 }
