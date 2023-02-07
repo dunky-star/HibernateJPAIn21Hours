276d606107a2ece58f2e825df7d359a45afb7f87
dunky-star <dukygeoffrey39@gmail.com>
2023-02-07 18:12:54 +0300

    Inheritance with JPA and Hibernate

--- src/main/java/com/dunky/in21hours/In21hoursApplication.java
+++ src/main/java/com/dunky/in21hours/In21hoursApplication.java
@@ -1,6 +1,9 @@
 package com.dunky.in21hours;
 
+import com.dunky.in21hours.entity.FullTimeEmployee;
+import com.dunky.in21hours.entity.PartTimeEmployee;
 import com.dunky.in21hours.repository.CourseRepository;
+import com.dunky.in21hours.repository.EmployeeRepository;
 import com.dunky.in21hours.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class In21hoursApplication implements CommandLineRunner {
@@ -23,6 +28,9 @@ public class In21hoursApplication implements CommandLineRunner {
 
 	@Autowired
 	private CourseRepository courseRepo;
+
+	@Autowired
+	private EmployeeRepository employeeRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -40,5 +48,14 @@ public class In21hoursApplication implements CommandLineRunner {
 
 		// Query all persons
         // logger.info("All user -> {}", jPaRepo.findAll());
+
+		employeeRepo.insert(new PartTimeEmployee("Duncan", new BigDecimal("50")));
+		employeeRepo.insert(new FullTimeEmployee("Sheena", new BigDecimal("10000")));
+
+		logger.info("Full Time Employees -> {}",
+				employeeRepo.retrieveAllFullTimeEmployees());
+
+		logger.info("Part Time Employees -> {}",
+				employeeRepo.retrieveAllPartTimeEmployees());
 	}
 }
---
+++ src/main/java/com/dunky/in21hours/repository/EmployeeRepository.java
@@ -0,0 +1,36 @@
+package com.dunky.in21hours.repository;
+
+import com.dunky.in21hours.entity.Employee;
+import com.dunky.in21hours.entity.FullTimeEmployee;
+import com.dunky.in21hours.entity.PartTimeEmployee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional
+public class EmployeeRepository {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EntityManager em;
+
+    public void insert(Employee employee) {
+        em.persist(employee);
+    }
+
+    public List<PartTimeEmployee> retrieveAllPartTimeEmployees() {
+        return em.createQuery("select e from PartTimeEmployee e", PartTimeEmployee.class).getResultList();
+    }
+
+    public List<FullTimeEmployee> retrieveAllFullTimeEmployees() {
+        return em.createQuery("select e from FullTimeEmployee e", FullTimeEmployee.class).getResultList();
+    }
+
+}
