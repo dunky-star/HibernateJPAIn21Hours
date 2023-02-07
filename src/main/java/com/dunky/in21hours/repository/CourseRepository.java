7deadf8834e25a9508977d6caca5e9ab1c32cf93
dunky-star <dukygeoffrey39@gmail.com>
2023-02-07 11:45:41 +0300

    OneToMany r/ship

--- src/main/java/com/dunky/in21hours/repository/CourseRepository.java
+++ src/main/java/com/dunky/in21hours/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package com.dunky.in21hours.repository;
 
 import com.dunky.in21hours.entity.Course;
+import com.dunky.in21hours.entity.Review;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -35,7 +37,32 @@ public class CourseRepository {
         return course;
     }
 
-    
+    public void deleteById(Long id) {
+        Course course = findById(id);
+        em.remove(course);
+    }
+
+    public void withEntityManager() {
+        Course course1 = new Course("Web Services in 100 Steps");
+        em.persist(course1);
+
+        Course course2 = findById(10001L);
+
+        course2.setName("JPA in 50 Steps - Updated");
+
+    }
+
+    public void addReviewsForCourse(Long courseId, List<Review> reviews) {
+        Course course = findById(courseId);
+        logger.info("course.getReviews() -> {}", course.getReviews());
+        for(Review review:reviews)
+        {
+            //setting the relationship
+            course.addReview(review);
+            review.setCourse(course);
+            em.persist(review);
+        }
+    }
 
 
 }
