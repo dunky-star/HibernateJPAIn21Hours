ad1df8eb7b211b69a582a49f63f41d4a2c8e5ab6
dunky-star <dukygeoffrey39@gmail.com>
2023-02-07 11:24:38 +0300

    OneToMany r/ship

--- src/main/java/com/dunky/in21hours/entity/Course.java
+++ src/main/java/com/dunky/in21hours/entity/Course.java
@@ -5,11 +5,12 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="course")
 public class Course {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -25,8 +26,39 @@ public class Course {
     @CreationTimestamp
     private LocalDateTime createdDate;
 
+    @OneToMany
+    private List<Review> reviews = new ArrayList<>();
+
     public Course(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(LocalDateTime lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        this.reviews.add(review);
+    }
+
+    public void removeReview(Review review) {
+        this.reviews.remove(review);
     }
 
     public String getName() {
--- src/main/java/com/dunky/in21hours/entity/Passport.java
+++ src/main/java/com/dunky/in21hours/entity/Passport.java
@@ -12,6 +12,9 @@ public class Passport {
     @Column(nullable = false)
     private String number;
 
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "passport")
+    private Student student;
+
     protected Passport() {
     }
     public Passport(String number) {
@@ -24,6 +27,15 @@ public class Passport {
     public void setNumber(String number) {
         this.number = number;
     }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
     public Long getId() {
         return id;
     }
