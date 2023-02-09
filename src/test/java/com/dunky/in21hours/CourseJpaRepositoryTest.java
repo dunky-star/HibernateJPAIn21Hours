43e9fe63ad62ebf229b90d8a1d28858577d23722
dunky-star <dukygeoffrey39@gmail.com>
2023-02-09 19:06:27 +0300

    JPQL Sort

--- src/test/java/com/dunky/in21hours/CourseJpaRepositoryTest.java
+++ src/test/java/com/dunky/in21hours/CourseJpaRepositoryTest.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -39,7 +42,21 @@ public class CourseJpaRepositoryTest {
 
     @Test
     public void sort() {
+        // Sort sort = new Sort(Sort.Direction.ASC, "name");
+        // logger.info("Sorted Courses -> {} ", repository.findAll(sort));
         List<Course> courses = repository.findAll(Sort.by(Sort.Direction.DESC, "name"));
         logger.info("Sorted Courses -> {} ", courses);
     }
+
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course> firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {} ", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page -> {} ", secondPage.getContent());
+    }
+
 }
